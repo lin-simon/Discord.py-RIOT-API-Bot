f4324a4eb987a0b877f2f7cf15db95a85407a0eb
lin-simon <88695972+lin-simon@users.noreply.github.com>
2022-01-13 15:08:11 -0500

    Update bot.py
    pushed changes for slash commands

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import os
 import asyncio
 import discord
+import league_scrape, val_scrape
 
 import selenium.common.exceptions
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+
 from multiprocessing.pool import ThreadPool
 
 opts = Options()
@@ -16,20 +20,40 @@ webdriver = webdriver.Chrome(options=opts)
 token = os.environ.get('bot_token')
 
 client = commands.Bot(command_prefix='?')
+slash = SlashCommand(client, sync_commands=True)
 pool = ThreadPool(processes=1)
+
 
 @client.event
 async def on_ready():
+    activity = discord.Game(name="straightest jojo fan")
+    await client.change_presence(activity=activity)
     print(f'{client.user} has connected to Discord!')
 
-@client.command()
-async def repeat(ctx, args, num):
-    for i in range(num):
+
+@slash.slash(
+    name="repeat",
+    description="repeats given string n-times"
+)
+async def _repeat(ctx: SlashContext, args, num):
+    for i in range(int(num)):
         await asyncio.sleep(float(num))
         await ctx.send(args)
 
-@client.command()
-async def league(ctx, username, region):
+
+@slash.slash(
+    name="test",
+    description="testing"
+)
+async def _bb(ctx: SlashContext):
+    await ctx.send('t')
+
+
+@slash.slash(
+    name="league",
+    description="currently broke as balls"
+)
+async def _league(ctx: SlashContext, username, region):
     name = username
     username = username.replace(" ", "%20")
     username = username.replace("#", "%23")
@@ -44,6 +68,7 @@ async def league(ctx, username, region):
         winrate = webdriver.find_element_by_css_selector('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierInfo > span.WinLose > span.winratio')
         wins = webdriver.find_element_by_css_selector('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierInfo > span.WinLose > span.wins')
         rank = webdriver.find_element_by_css_selector('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierRank')
+
         rank = rank.text
 
         if rank.lower() == 'challenger' or rank.lower() == 'master' or rank.lower() == 'grandmaster':
@@ -53,39 +78,22 @@ async def league(ctx, username, region):
         embed.add_field(name="Wins", value=wins.text.strip('W'))
         embed.add_field(name='Win Rate', value=winrate.text[9:])
         embed.set_image(url=f'https://opgg-static.akamaized.net/images/medals/{rank.replace(" ","_").lower()}.png?image=q_auto:best&v=1')
-
         await ctx.send(embed=embed)
 
     except selenium.common.exceptions.NoSuchElementException:
         await ctx.send(f'{name} is unranked or does not exist D:')
 
 
-
-@client.command()
-async def val(ctx, username, *gamemode):
-    username = username.replace(" ", "%20")
-    username = username.replace("#", "%23")
-    embed = discord.Embed()
-
-    if "unrated" in gamemode:
-        webdriver.get(f'https://tracker.gg/valorant/profile/riot/{username}/overview?playlist=unrated&season=all')
-    else:
-        webdriver.get(f'https://tracker.gg/valorant/profile/riot/{username}/overview?playlist=competitive&season=all')
-        rank = webdriver.find_element_by_css_selector('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.site-container.trn-grid.trn-grid--vertical.trn-grid--small > div.trn-grid.container > div.segment-stats.area-main-stats.card.bordered.header-bordered.responsive > div.highlighted.highlighted--giants > div.highlighted__content > div > div.valorant-highlighted-content__stats > img')
-
-
-    wins = webdriver.find_element_by_css_selector('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.site-container.trn-grid.trn-grid--vertical.trn-grid--small > div.trn-grid.container > div.segment-stats.area-main-stats.card.bordered.header-bordered.responsive > div.main > div.stat.align-left.expandable.feature-hint > div > div.numbers > span.value')
-    winrate = webdriver.find_element_by_css_selector('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.site-container.trn-grid.trn-grid--vertical.trn-grid--small > div.trn-grid.container > div.segment-stats.area-main-stats.card.bordered.header-bordered.responsive > div.giant-stats > div:nth-child(4) > div > div.numbers > span.value')
-    #accuracy = webdriver.find_element_by_css_selector('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.site-container.trn-grid.trn-grid--vertical.trn-grid--small > div.trn-grid.container > div.accuracy.accuracy > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2)')
-    tag = webdriver.find_element_by_css_selector('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.ph > div.ph__container > div.ph-details > div.ph-details__identifier > span > span.trn-ign__discriminator')
-    name = webdriver.find_element_by_css_selector('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.ph > div.ph__container > div.ph-details > div.ph-details__identifier > span > span.trn-ign__username')
-
-    embed = discord.Embed(title=name.text, description=tag.text)
-    embed.add_field(name="Wins", value=wins.text)
-    embed.add_field(name="Win %", value=winrate.text)
-    #embed.add_field(name="Headshot %", value=accuracy.text)
-    embed.set_image(url=rank.get_attribute('src'))
-
-    await ctx.send(embed=embed)
+@slash.slash(
+    name="val",
+    description="A valorant competitive profile lookup, args: {username}#{region} ex. slimslam#NA1"
+)
+async def _val(ctx: SlashContext, username):
+    try:
+        embed = val_scrape.val(username)
+        await ctx.send(embed=embed)
+    except discord.errors.NotFound:
+        await ctx.send('Something went wrong, try again (404 Not Found (error code: 10062): Unknown interaction')
 
 client.run(token)
+
